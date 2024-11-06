1faec8cdd628dff34676b7f72baff1030ded5aaa
hienkyle <hien2003.dm@gmail.com>
2024-11-06 10:13:29 -0600

    fix merge conflict

--- psycworks/app/(main)/assessments/new/page.tsx
+++ psycworks/app/(main)/assessments/new/page.tsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-=======
 import { CreationForm } from "@/components/assessments/creation-form";
->>>>>>> main
-
 
 export default function NewAssessmentPage() {
   return (
     <div>
-<<<<<<< HEAD
-      NewAssessmentPage
-=======
-     
+
       <CreationForm />
-     
->>>>>>> main
+
     </div>
   );
 }
