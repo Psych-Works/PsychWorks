1f5cbc649d43d48e340d457a1444d2ed5f56ab66
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2024-11-08 10:44:40 -0600

    Feat: Integrate assessments table to page

--- psycworks/app/(main)/assessments/page.tsx
+++ psycworks/app/(main)/assessments/page.tsx
@@ -1,6 +1,7 @@
-import React from "react"
-import { Button } from "@/components/ui/button"
-import { SearchBar } from "@/components/searchbar/search-bar"
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { SearchBar } from "@/components/searchbar/search-bar";
+import { AssessmentsTable } from "@/components/assessments/assessments-table";
 
 export default function AssessmentsPage() {
   return (
@@ -8,13 +9,17 @@ export default function AssessmentsPage() {
       <div className="absolute left-[10%] right-[10%] top-40 flex justify-between items-center">
         <div>
           <h1 className="text-4xl font-bold">Assessment Table Template</h1>
-          <p className="text-gray-500 mt-2">Create templates to use tables that you can reuse on Templates</p>
+          <p className="text-gray-500 mt-2">
+            Create templates to use tables that you can reuse on Templates
+          </p>
         </div>
         <Button className="w-40 h-12">Create</Button>
       </div>
       <div className="absolute left-40 right-40 top-60">
         <SearchBar />
       </div>
+
+      <AssessmentsTable />
     </div>
   );
 }
