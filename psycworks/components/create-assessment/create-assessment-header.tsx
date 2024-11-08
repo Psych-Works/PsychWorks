b5ab8ef70e3a7e3817ccf46cdd39618ed233874e
hienkyle <hien2003.dm@gmail.com>
2024-11-08 10:03:15 -0600

    add component to the create assessment page

--- psycworks/components/create-assessment/create-assessment-header.tsx
+++ psycworks/components/create-assessment/create-assessment-header.tsx
@@ -1,14 +1,23 @@
 'use client'
 
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup, DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
-import {Button} from "@/components/ui/button";
+// import {
+//     DropdownMenu,
+//     DropdownMenuContent,
+//     DropdownMenuGroup, DropdownMenuItem,
+//     DropdownMenuLabel,
+//     DropdownMenuTrigger
+// } from "@/components/ui/dropdown-menu";
+// import {Button} from "@/components/ui/button";
 import {useState} from "react";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue
+} from "@/components/ui/select";
 
 export default function CreateAssessmentHeader(){
     const [selectedItem, setSelectedItem] = useState(null);
@@ -17,9 +26,9 @@ export default function CreateAssessmentHeader(){
         setSelectedItem(item);
     }
     const types = [ // will get this from server
-        {id: 1, label: 'Type 1'},
-        {id: 2, label: 'Type 2'},
-        {id: 3, label: 'Type 3'}
+        {id: 1, value: 'Type 1'},
+        {id: 2, value: 'Type 2'},
+        {id: 3, value: 'Type 3'}
     ];
 
     return (
@@ -32,21 +41,37 @@ export default function CreateAssessmentHeader(){
                 <p className={"col-start-2 col-end-3 mx-10 text-black font-extrabold text-xl" +
                     " justify-self-center"}>Table type:</p>
                 <p className={'col-start-3 col-end-5 w-full'}>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button className={'w-full text-lg'}>{selectedItem || 'Select a type'}</Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className={"w-56"}>
-                            <DropdownMenuLabel>Table types</DropdownMenuLabel>
-                            <DropdownMenuGroup>
+                    {/*<DropdownMenu>*/}
+                    {/*    <DropdownMenuTrigger asChild>*/}
+                    {/*        <Button className={'w-full text-lg'}>{selectedItem || 'Select a type'}</Button>*/}
+                    {/*    </DropdownMenuTrigger>*/}
+                    {/*    <DropdownMenuContent className={"w-56"}>*/}
+                    {/*        <DropdownMenuLabel>Table types</DropdownMenuLabel>*/}
+                    {/*        <DropdownMenuGroup>*/}
+                    {/*            {types.map((item) => (*/}
+                    {/*                <DropdownMenuItem key={item.id} textValue={item.value} onSelect={() => select(item.value)}>*/}
+                    {/*                    {item.value}*/}
+                    {/*                </DropdownMenuItem>*/}
+                    {/*            ))}*/}
+                    {/*        </DropdownMenuGroup>*/}
+                    {/*    </DropdownMenuContent>*/}
+                    {/*</DropdownMenu>*/}
+
+                    <Select>
+                        <SelectTrigger>
+                            <SelectValue placeholder={'Select a type'} className={'w-full !text-3xl'}></SelectValue>
+                        </SelectTrigger>
+                        <SelectContent className={"w-full"}>
+                            <SelectGroup>
+                                <SelectLabel className={'text-2xl'}>Table types</SelectLabel>
                                 {types.map((item) => (
-                                    <DropdownMenuItem key={item.id} onSelect={() => select(item.label)}>
-                                        {item.label}
-                                    </DropdownMenuItem>
+                                    <SelectItem key={item.id} value={item.value} onSelect={() => select(item.value)}>
+                                        {item.value}
+                                    </SelectItem>
                                 ))}
-                            </DropdownMenuGroup>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
                 </p>
             </div>
         </>
