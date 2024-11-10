747d197a5fd2ca32a32d859999770871d4827a83
RRAndrade1 <r.r.andrade@tcu.edu>
2024-11-10 12:16:56 -0600

    Fix: Updated page under new to properyl run component

--- psycworks/app/(main)/assessments/new/page.tsx
+++ psycworks/app/(main)/assessments/new/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+import { useState } from "react";
 import { CreationForm } from "@/components/assessments/creation-form";
 
 
 export default function NewAssessmentPage() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div>
      
-      <CreationForm />
+     <CreationForm 
+        isOpen={isOpen} 
+        onOpenChange={setIsOpen}
+      />
      
     </div>
   );
