f392dcbf0ea9f6014272f5c99ca9a93e46d47a96
hienkyle <hien2003.dm@gmail.com>
2024-11-10 11:44:54 -0600

    fix: correct the path to assessment creation

--- psycworks/app/(main)/assessments/new/page.tsx
+++ psycworks/app/(main)/assessments/new/page.tsx
@@ -1,11 +1,26 @@
 import { CreationForm } from "@/components/assessments/creation-form";
-
+import {Button} from "@/components/ui/button";
+import CreateAssessmentHeader from "@/components/assessments/create-assessment-header";
+import CreateAssessmentField from "@/components/assessments/create-assessment-field";
 
 export default function NewAssessmentPage() {
   return (
     <div>
-     
-      <CreationForm />
+     <div className="space-y-20">
+            <div className="flex-col items-center justify-items-center">
+                <CreateAssessmentHeader/>
+                <CreateAssessmentField name='Name' type='text'/>
+                <CreateAssessmentField name='Measure' type='text'/>
+                <div className='grid grid-cols-5 w-full'>
+                    <CreationForm />
+                </div>
+                <div className='grid grid-cols-5 w-full fixed bottom-10 left-10'>
+                    <Button className='col-start-1 col-span-1'>Cancel</Button>
+                </div>
+            </div>
+
+        </div>
+      
      
     </div>
   );
