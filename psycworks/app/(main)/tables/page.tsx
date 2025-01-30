1b2d2e1085c078018991fcee1948e3633c4ba706
RyanPNSmith <ryanpsmith2003@outlook.com>
2025-01-30 10:43:02 -0600

    Created page for Table Modification
    Created route between modify icon on tables and the new page used to modify the tables.

---
+++ psycworks/app/(main)/tables/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return (
+        <div className="container mx-auto space-y-8 py-8">
+            <div className="flex justify-between items-center">
+                <div>
+                    <h1 className="text-4xl font-bold">Modfy Tables</h1>
+                    <p className="text-gray-500 mt-2">
+                        Modify crated tabels
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Page;
