e8034fcdf2c3e15a5199c0bd8844a4911307af77
RRAndrade1 <r.r.andrade@tcu.edu>
2025-02-23 11:57:20 -0600

    Fix: Added file-save dependency, imported docx-example

--- psycworks/components/assessments/report-gen/static-report-text.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import * as docx from "docx";
-
-const doc = new docx.Document({
-    sections: [
-        
-    ]
-});
---
+++ psycworks/components/reports/report-gen/static-report-text.tsx
@@ -0,0 +1,150 @@
+import React from 'react';
+import { Document, Packer, Paragraph, TextRun, Header, ImageRun, ExternalHyperlink } from 'docx';
+
+export const ReportTitle = new Paragraph({
+    alignment: 'center',
+    children: [
+        new TextRun({
+            text: 'NEUROPSYCHOLOGICAL EVALUATION',
+            break: 1,
+            bold: true,
+            size: 24,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: 'CONFIDENTIAL',
+            break: 1,
+            bold: true,
+            size: 24,
+            font: 'Times New Roman',
+            color: '#FF0000',
+        })
+    ]
+});
+
+export const ClientInfo = new Paragraph({
+    alignment: 'left',
+    children: [
+        new TextRun({
+            text: 'Name: ',
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: '\t',
+
+        }),
+        new TextRun({
+            text: 'Date(s) of Evaluation: ',
+            break: 1,
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: 'Date of Birth: ',
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: '\t',
+        }),
+        new TextRun({
+            text: 'Date of Report: ',
+            break: 1,
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: 'Age: ',
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: '\t',
+        }),
+        new TextRun({
+            text: 'Evaluator: ',
+            break: 1,
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: 'Insurance: ',
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+        new TextRun({
+            text: '\t',
+        }),
+        new TextRun({
+            text: 'Referral: ',
+            bold: true,
+            size: 12,
+            font: 'Times New Roman',
+        }),
+    ]
+});
+
+export const ReportHeader = new Header({
+    children: [
+        // new Paragraph({
+        //     children: [
+        //         new ImageRun({
+        //             type: 'png',
+        //             data: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+        //             transformation: {
+        //                 width: 100,
+        //                 height: 100,
+        //             },
+        //         }),
+        //     ],
+        // }),
+        new Paragraph({
+            alignment: 'left',
+            children: [
+                new TextRun({
+                    text: '300 College Ave, Fort Worth, Texas 76104',
+                    break: 1,
+                    bold: true,
+                    size: 20,
+                    font: 'Times New Roman',
+                    color: '#808080',
+                }),
+                new TextRun({
+                    text: 'Office: 817.394.7646',
+                    break: 1,
+                    bold: true,
+                    size: 20,
+                    font: 'Times New Roman',
+                    color: '#808080',
+                }),
+                new TextRun({
+                    text: 'Fax: 817.631.2405',
+                    break: 1,
+                    bold: true,
+                    size: 20,
+                    font: 'Times New Roman',
+                    color: '#808080',
+                }),
+                new ExternalHyperlink({
+                    children: [
+                        new TextRun({
+                            text: 'www.fwpsychworks.com',
+                            style: 'hyperlink',
+                        }),
+                    ],
+                    link: 'https://www.fwpsychworks.com/',
+                }),
+            ],
+        }),
+    ]
+});
+
+
