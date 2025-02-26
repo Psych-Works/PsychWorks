de0fbf769722e94a08d0cff61c5e8c99ec2295cf
Sion Kim <48657598+sionkim00@users.noreply.github.com>
2025-02-26 09:52:15 -0600

    Refactor: naming consistency

--- psycworks/components/reports/export-to-docx-button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-
-const ExportToDocxButton = () => {
-  return <Button className="w-40 h-12">Export to Docx</Button>;
-};
-
-export default ExportToDocxButton;
